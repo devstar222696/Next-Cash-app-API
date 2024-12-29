1a8986a53360f9b24421fc305085f4bbb0cd828c
islandHouse <Islandhouse2000@gmail.com>
2024-12-28 22:50:06 -1000

    Update user-register-listing-page.tsx

--- sections/userregister/views/user-register-listing-page.tsx
+++ sections/userregister/views/user-register-listing-page.tsx
@@ -17,10 +17,21 @@ export default async function UserRegisterListingPage({}: TEmployeeListingPage) 
       <div className="space-y-4">
         <Breadcrumbs items={breadcrumbItems} />
 
+        {/* 헤딩 영역 */}
         <div className="flex items-start justify-between">
           <Heading title={`Register`} description="" />
         </div>
+
+        {/* 안내문 영역 */}
+        <div className="text-center">
+          <p className="text-sm text-muted-foreground">
+            We cannot process register requests for two days. We will fix it as soon as possible.
+          </p>
+        </div>
+
         <Separator />
+
+        {/* 테이블 영역 */}
         <UserRegisterTable />
       </div>
     </PageContainer>
