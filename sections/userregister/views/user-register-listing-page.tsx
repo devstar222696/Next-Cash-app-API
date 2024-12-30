667a84d2400e9fe3be7de1805236b41c51d7044b
Vimal Bhalani <vimalbhalani77@gmail.com>
2024-12-30 18:59:02 +0530

    prevent register page without login

--- sections/userregister/views/user-register-listing-page.tsx
+++ sections/userregister/views/user-register-listing-page.tsx
@@ -1,8 +1,11 @@
+'use client';
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import UserRegisterTable from '../register-tables';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const breadcrumbItems = [
   { title: 'MyPage', link: '/mypage' },
@@ -11,7 +14,19 @@ const breadcrumbItems = [
 
 type TEmployeeListingPage = {};
 
-export default async function UserRegisterListingPage({}: TEmployeeListingPage) {
+export default function UserRegisterListingPage({}: TEmployeeListingPage) {
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (localStorage && typeof localStorage !== 'undefined') {
+      const userInfoStr = localStorage.getItem('userinfo');
+      if (!userInfoStr) {
+        router.push('/');
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <PageContainer scrollable>
       <div className="space-y-4">
