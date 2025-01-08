1c13d0723c4dfad48db3544dffca584fe6d3344b
Vimal Bhalani <vimalbhalani77@gmail.com>
2025-01-08 09:17:10 +0530

    - remove console log

--- app/api/admin/seasongame/route.ts
+++ app/api/admin/seasongame/route.ts
@@ -19,3 +19,29 @@ export const GET = async (request: NextRequest) => {
     );
   }
 };
+
+export const POST = async (request: NextRequest) => {
+  const { games } = await request.json();
+  await dbConnect();
+
+  try {
+    const seasonGames = games.map((game: any) => ({
+      updateOne: {
+        filter: { _id: game.id },
+        update: { $set: { isfavourite: game.isfavourite } },
+      },
+    }));
+    
+    await SeasonGame.bulkWrite(seasonGames);
+
+    return NextResponse.json(
+      { ok: 'Season game favorite successfully' },
+      { status: 200 }
+    );
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: 'An error occurred while favorite season game' },
+      { status: 500 }
+    );
+  }
+};
