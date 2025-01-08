f9aff5e3d058ade0f44728183dce2edcd68b0d5b
Vimal Bhalani <vimalbhalani77@gmail.com>
2025-01-08 17:52:44 +0530

    Add House Rule image and Replace House Rule Text as per IH update 3.drawio Requirement

--- sections/promotion/promotion-houseRule/index.tsx
+++ sections/promotion/promotion-houseRule/index.tsx
@@ -1,24 +1,34 @@
+import Image from 'next/image';
 import React from 'react';
 
 const houseRules = [
   'Withdrawal Rule',
-  '$20 or more Deposited Player - Minimum request for $50 and Daily max amount $3,000',
-  'Free Player - To apply for Redeem, users need to have a spin record of at least $100.',
-  ' The minimum amount that can be requested is $100, and the maximum amount that can be withdrawn per day is also $100.',
-  'Your tag number is your ID. Please be sure to remember it.',
-  'We do not ask for tips, and we do not accept also.',
-  'Referral Bonus, Daily Bonus, and First Deposit Bonus can only be received when the User deposits more than $10.',
-  'All bonuses are unstackable and cannot be received in one time.',
-  'Withdrawal requires a minimum number of spins equal to the deposit amount',
-  "Only users who have deposited more than $10 are considered 'Old Users'.",
-  'The minimum deposit is $1, but we don’t accept cents. Dollars only.',
-  'The minimum Cashout is $50, the maximum is $3,000, and if exceeded, User can apply again the next day.',
-  'All daily bonuses reset at: 00:00 HST time, 02:00 PST time, and 04:00 CST time.'
+  'Your tag number is like a your ID. Please be sure to remember it.',
+  'DAILY $2 FREEPLAY and 20$ BONUS only available to VIP Players who have made deposits of $10 or more.',
+  'Free players who have not made deposits of $10 or more must have a spin record of over $100 per game account to make a redeem request. Additionally, only up to $100 can be redeemed per day.',
+  'Players who deposit $10 or more can receive a 100% First Deposit BONUS and be upgraded to VIP status.',
+  'When a player you referred makes their first deposit of $10 or more, you can receive a 50% bonus of that amount.',
+  'All bonuses cannot be claimed at once. Only one bonus can be received per recharge.',
+  'Redeem request requires a minimum number of spins equal to the deposit amount.',
+  'All daily bonuses reset at 00:00 HST time, 02:00 PST time, and 04:00 CST time.',
+  "All deposit and withdrawal history is kept for up to 2 weeks. This is the game company's recommendation.",
+  'The Daily maximum Cashout is $3,000, and if it is exceeded, User can apply again the next day.',
+  "The minimum deposit is $1, but we don't accept cents Dollars Only.",
+  'We do not ask for tips, and we do not accept also.'
 ];
 
 const HouseRules = () => (
   <div className="mx-auto max-w-3xl rounded-lg  p-6 ">
-    <h2 className="mb-6 text-center text-2xl font-semibold">House Rules</h2>
+    {/* <h2 className="mb-6 text-center text-2xl font-semibold">House Rules</h2>  */}
+    <div className="mb-4 flex justify-center">
+      <Image
+        src="/IH house rule title.png"
+        width={500}
+        height={200}
+        className="mt-1 hover:opacity-80 lg:ml-2 lg:mt-0"
+        alt="house rule"
+      ></Image>
+    </div>
     <div className="rounded-lg  ">
       <ul className="space-y-4 text-sm sm:text-base md:text-lg">
         {houseRules.map((rule, index) => (
