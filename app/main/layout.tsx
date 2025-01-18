ca34c5b64fccf7488c021f6dc884a785f1f5d177
islandHouse <Islandhouse2000@gmail.com>
2025-01-18 12:55:58 -1000

    Update layout.tsx

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
+import { SidebarProvider } from '@/components/layout/sidebar-context';
+
 export default function DashboardLayout({
   children
 }: {
@@ -25,6 +27,7 @@ export default function DashboardLayout({
           router.push('/');
         }
       } catch (error) {
+        console.error('Auth check error:', error);
         router.push('/');
       }
     };
@@ -33,12 +36,14 @@ export default function DashboardLayout({
   }, [router]);
 
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="w-full flex-1 overflow-hidden">
-        <Header />
-        {children}
-      </main>
-    </div>
+    <SidebarProvider>
+      <div className="flex">
+        <Sidebar />
+        <main className="w-full flex-1 overflow-hidden">
+          <Header />
+          {children}
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
