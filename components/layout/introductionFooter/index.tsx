d089d940a93e9ba12911bf552b10c8798b21461a
Vimal Bhalani <vimalbhalani77@gmail.com>
2025-01-20 18:03:54 +0530

    Add Logo to Footer

--- components/layout/introductionFooter/index.tsx
+++ components/layout/introductionFooter/index.tsx
@@ -24,19 +24,38 @@ const IntroductionFooter = () => {
                   alt="ad"
                   className=""
                 />
+                 <Image
+                  src="/social/001.png"
+                  width={24}
+                  height={18}
+                  alt="ad"
+                  className=""
+                />
+                 <Image
+                  src="/social/002.png"
+                  width={24}
+                  height={18}
+                  alt="ad"
+                  className=""
+                />
               </div>
             </div>
             <p className="mt-2 text-sm font-medium">
              2024 - 2025 Island House. All Rights Reserved.
             </p>
-            <Image src="/IH Title.png" width={160} height={160} alt="ad" />
+            {/* <Image src="/IH Title.png" width={160} height={160} alt="ad" /> */}
+            <Image src="/IH-LOGO.png" width={170} height={170} alt="logo image" />
+
             <div className="mt-3 flex gap-0">
-              <Image src="/Visa.png" width={36} height={36} alt="Visa" />
               <Image src="/Dollar.png" width={36} height={36} alt="Dollar" />
-              <Image src="/Zelle.png" width={36} height={36} alt="Zelle" />
-              <Image src="/Bitcoins.png" width={34} height={36} alt="Bitcoin" />
+              <Image src="/Visa.png" width={36} height={36} alt="Visa" />
               <Image src="/PayPal.png" width={36} height={36} alt="PayPal" />
-              <Image src="/usdt.png" width={36} height={36} alt="USDT" />
+              <Image src="/Bitcoins.png" width={34} height={36} alt="Bitcoin" />
+              <Image src="/payment/TR.png" width={34} height={36} alt="TR" />
+              <Image src="/payment/ETH.png" width={34} height={36} alt="ETH" />
+              <Image src="/usdt.png" width={36} height={36} alt="USDT" /> 
+              {/* <Image src="/Zelle.png" width={36} height={36} alt="Zelle" /> */}
+              <Image src="/payment/USDC.png" width={36} height={36} alt="USDC" /> 
             </div>
           </div>
         </div>
