bba0024da672265d185bcd3eddfb24d304dfa36a
islandHouse <Islandhouse2000@gmail.com>
2025-01-22 09:59:04 -1000

    Update breadcrumbs.tsx

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -31,26 +31,30 @@ const sliderSettings = {
   slidesToScroll: 1,
   autoplay: true,
   adaptiveHeight: false,
-  // appendDots: true,
   speed: 500,
   autoplaySpeed: 3000,
   cssEase: "linear",
   appendDots: (dots: any) => (
-    <div
-      className='p-0 m-0 text-black dark:text-white'
-    >
+    <div className='p-0 m-0 text-black dark:text-white'>
       <ul style={{ margin: "0px" }}> {dots} </ul>
-    </div>)
-}
+    </div>
+  )
+};
 
 const posterImages = [
   "/promo/main_poster_2.png",
   "/promo/main_poster_3.png",
   "/promo/main_poster_4.png",
   "/promo/main_poster_5.png",
+];
 
-]
-export function Breadcrumbs({ items, showBreadcrumbs = true }: { items: BreadcrumbItemProps[], showBreadcrumbs?: boolean }) {
+export function Breadcrumbs({
+  items,
+  showBreadcrumbs = true
+}: {
+  items: BreadcrumbItemProps[],
+  showBreadcrumbs?: boolean
+}) {
   const router = useRouter();
   const pathName = usePathname();
 
@@ -60,49 +64,64 @@ export function Breadcrumbs({ items, showBreadcrumbs = true }: { items: Breadcru
 
   return (
     <Breadcrumb>
-      {UserRoles.includes(userInfo.role) ?
+      {UserRoles.includes(userInfo.role) && (
         <div>
-          {pathName === '/mypage' ? 
-          (
-          <Slider {...sliderSettings} className='grid mb-10'>
-            {posterImages.map((img, index) => <img src={img} key={img} width={1000} height={500} className="w-full promo-image" alt="cover" />)}
-          </Slider>
-          ) 
-          : 
-          <Image src="/promo/web-cover.png" width={1000} height={500} className="mb-10 w-full" alt="cover" />
-          }
+          {pathName === '/mypage' && (
+            <Slider {...sliderSettings} className='grid mb-10'>
+              {posterImages.map((img, index) => (
+                <img
+                  src={img}
+                  key={img}
+                  width={1000}
+                  height={500}
+                  className="w-full promo-image"
+                  alt="cover"
+                />
+              ))}
+            </Slider>
+          )}
+
+          {pathName === '/mypage/chat' && (
+            <Image
+              src="/promo/web-cover.png"
+              width={1000}
+              height={500}
+              className="mb-10 w-full"
+              alt="cover"
+            />
+          )}
 
           <Image
             src="/chat-image.png"
             width={70}
             height={70}
             alt="chatting"
-            className="absolute right-5 bottom-[5px] sm:bottom-[10Fpx] z-50 cursor-pointer hover:scale-105 sm:right-14"
+            className="absolute right-5 bottom-[5px] sm:bottom-[10px] z-50 cursor-pointer hover:scale-105 sm:right-14"
             onClick={chatting}
           />
         </div>
-        : ""
-      }
+      )}
+
       {showBreadcrumbs && (
-      <BreadcrumbList>
-        {items.map((item, index) => (
-          <Fragment key={item.title}>
-            {index !== items.length - 1 && (
-              <BreadcrumbItem>
-                <BreadcrumbLink href={item.link}>{item.title}</BreadcrumbLink>
-              </BreadcrumbItem>
-            )}
-            {index < items.length - 1 && (
-              <BreadcrumbSeparator>
-                <Slash />
-              </BreadcrumbSeparator>
-            )}
-            {index === items.length - 1 && (
-              <BreadcrumbPage>{item.title}</BreadcrumbPage>
-            )}
-          </Fragment>
-        ))}
-      </BreadcrumbList>
+        <BreadcrumbList>
+          {items.map((item, index) => (
+            <Fragment key={item.title}>
+              {index !== items.length - 1 && (
+                <BreadcrumbItem>
+                  <BreadcrumbLink href={item.link}>{item.title}</BreadcrumbLink>
+                </BreadcrumbItem>
+              )}
+              {index < items.length - 1 && (
+                <BreadcrumbSeparator>
+                  <Slash />
+                </BreadcrumbSeparator>
+              )}
+              {index === items.length - 1 && (
+                <BreadcrumbPage>{item.title}</BreadcrumbPage>
+              )}
+            </Fragment>
+          ))}
+        </BreadcrumbList>
       )}
     </Breadcrumb>
   );
